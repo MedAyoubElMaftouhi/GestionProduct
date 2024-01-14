8553f2a31093dad8e6a1476848f56b9ba475dc6f
DOUHI El Mehdi <douhielmehdi@gmail.com>
2024-01-14 22:12:39 +0100

    create test avec class exception

--- src/main/java/ehei/ma/GestionProduct/ProduitService.java
+++ src/main/java/ehei/ma/GestionProduct/ProduitService.java
@@ -5,12 +5,28 @@ import java.util.Map;
 
 public class ProduitService {
 
-	 private Map<Long, Produit> produits = new HashMap<>();
+    private Map<Long, Produit> produits = new HashMap<>();
 
-	 // Read
-	    public Produit getProduit(Long id) {
-	        return produits.get(id);
-	    }
+    // Read
+    public Produit getProduit(Long id) {
+        return produits.get(id);
+    }
 
-	
+    // Create
+    public void ajouterProduit(Produit produit) throws ProduitExistException, IllegalArgumentException {
+        verifierUniciteEtValidite(produit);
+        produits.put(produit.getId(), produit);
+    }
+
+    private void verifierUniciteEtValidite(Produit nouveauProduit) throws ProduitExistException, IllegalArgumentException {
+        for (Produit existantProduit : produits.values()) {
+            if (existantProduit.getId().equals(nouveauProduit.getId()) || existantProduit.getNom().equals(nouveauProduit.getNom())) {
+                throw new ProduitExistException("Un produit avec le même ID ou nom existe déjà.");
+            }
+        }
+
+        if (nouveauProduit.getPrix() < 0 || nouveauProduit.getQuantite() < 0) {
+            throw new IllegalArgumentException("Le prix et la quantité doivent être positifs.");
+        }
+    }
 }
