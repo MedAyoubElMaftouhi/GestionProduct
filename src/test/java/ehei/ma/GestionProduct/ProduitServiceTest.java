0b30c090631ccef8b8d3eca23ca52bbc75a55b0f
DOUHI El Mehdi <douhielmehdi@gmail.com>
2024-01-14 23:03:00 +0100

    importations

--- src/test/java/ehei/ma/GestionProduct/ProduitServiceTest.java
+++ src/test/java/ehei/ma/GestionProduct/ProduitServiceTest.java
@@ -1,5 +1,12 @@
 package ehei.ma.GestionProduct;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
