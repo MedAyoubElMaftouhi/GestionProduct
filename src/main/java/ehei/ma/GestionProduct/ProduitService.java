3b501e16c1b6fc925cf553ece33c67879ff040a9
DOUHI El Mehdi <douhielmehdi@gmail.com>
2024-01-14 21:47:33 +0100

    map test

--- src/main/java/ehei/ma/GestionProduct/ProduitService.java
+++ src/main/java/ehei/ma/GestionProduct/ProduitService.java
@@ -1,5 +1,11 @@
 package ehei.ma.GestionProduct;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProduitService {
 
+	 private Map<Long, Produit> produits = new HashMap<>();
+
+	
 }
